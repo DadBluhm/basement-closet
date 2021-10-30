82058876d0fe184499cbdb69b95a701067fb3914
Roger <rbluhm@rocketmail.com>
2021-10-29 21:12:55 -0500

    Add DB DDL

--- basement_closet/__init__.py
+++ basement_closet/__init__.py
@@ -1,13 +1,53 @@
 from pydantic import BaseModel, Field
 from uuid import uuid4
 from typing import MutableMapping, Optional
-
+import databases
+import sqlalchemy
 from fastapi import FastAPI
-from asyncmy import connect
+
 from os import getenv
 
 app = FastAPI()
 data_store: MutableMapping[str, "Item"] = {}
+
+user=getenv("DBUSER")
+password=getenv("DBPWD")
+host=getenv("DBHOST")
+port=int(getenv("DBPORT", "3306"))
+dbname=getenv("DBNAME")
+
+DATABASE_URL=f"mysql://{user}:{password}@{host}:{port}/{dbname}"
+database=databases.Database(DATABASE_URL)
+
+metadata = sqlalchemy.MetaData()
+
+inventory = sqlalchemy.Table(
+    "inventory",
+    metadata,
+    sqlalchemy.Column("invID", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("invName", sqlalchemy.String),
+    sqlalchemy.Column("invType", sqlalchemy.String),
+    sqlalchemy.Column("invDescription", sqlalchemy.String),
+    sqlalchemy.Column("age", sqlalchemy.String),
+    sqlalchemy.Column("shelf", sqlalchemy.String),
+    sqlalchemy.Column("bin", sqlalchemy.String),
+    sqlalchemy.Column("location", sqlalchemy.String)
+)
+
+inventory_comments = sqlalchemy.Table(
+    "inventory_comments",
+    metadata,
+    sqlalchemy.Column("invID", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("commentID", sqlalchemy.String),
+    sqlalchemy.Column("entryDate", sqlalchemy.DateTime),
+    sqlalchemy.Column("enteredBy", sqlalchemy.String),
+    sqlalchemy.Column("comment", sqlalchemy.String)
+)
+
+engine = sqlalchemy.create_engine(
+    DATABASE_URL, connect_args={"check_same_thread": False}
+)
+metadata.create_all(engine, checkfirst=True)
 
 class Item(BaseModel):
     id: str = Field(default_factory=lambda: str(uuid4()))
@@ -21,14 +61,13 @@ class Item(BaseModel):
 
 @app.on_event("startup")
 async def startup_event():
-    connection=await connect(
-        user=getenv("DBUSER"),
-        password=getenv("DBPWD"),
-        host=getenv("DBHOST"),
-        port=int(getenv("DBPORT")),
-        database=getenv("DBNAME")
-    )
+    await database.connect()
+
     print("Connected to database")
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 @app.put("/inventory")
 def create(item: Item):
