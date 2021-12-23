6baa511376f9152c3439c4b94bd35b4fe410bae6
Daniel Bluhm <dbluhm@pm.me>
2021-12-23 12:27:10 -0500

    feat: foreign key relationships
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- basement_closet/__init__.py
+++ basement_closet/__init__.py
@@ -1,11 +1,13 @@
 from pydantic import BaseModel, Field
-from uuid import uuid4
 from typing import MutableMapping, Optional
 import databases
 import sqlalchemy
 from fastapi import FastAPI
+from datetime import datetime
 
 from os import getenv
+
+from sqlalchemy.sql.schema import ForeignKey
 
 app = FastAPI()
 data_store: MutableMapping[str, "Item"] = {}
@@ -37,8 +39,10 @@ inventory = sqlalchemy.Table(
 inventory_comments = sqlalchemy.Table(
     "inventory_comments",
     metadata,
-    sqlalchemy.Column("invID", sqlalchemy.Integer, primary_key=True),
-    sqlalchemy.Column("commentID", sqlalchemy.String(30)),
+    sqlalchemy.Column("commentID", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column(
+        "invID", sqlalchemy.Integer, ForeignKey("inventory.invID"), primary_key=True
+    ),
     sqlalchemy.Column("entryDate", sqlalchemy.DateTime),
     sqlalchemy.Column("enteredBy", sqlalchemy.String(30)),
     sqlalchemy.Column("comment", sqlalchemy.String(512)),
@@ -48,21 +52,23 @@ engine = sqlalchemy.create_engine(DATABASE_URL)
 metadata.create_all(engine, checkfirst=True)
 
 
-class Item(BaseModel):
-    id: str = Field(default_factory=lambda: str(uuid4()))
-    name: str
-    type: str
-    description: str
+class ItemIn(BaseModel):
+    name: str = Field(alias="invName")
+    type: str = Field(alias="invType")
+    description: str = Field(alias="invDescription")
     age: Optional[int] = None
     shelf: Optional[str] = None
     bin: Optional[str] = None
     location: Optional[str] = None
 
 
+class Item(ItemIn):
+    id: int = Field(alias="invID")
+
+
 @app.on_event("startup")
 async def startup_event():
     await database.connect()
-
     print("Connected to database")
 
 
@@ -72,28 +78,69 @@ async def shutdown():
 
 
 @app.put("/inventory")
-def create(item: Item):
-    data_store[item.id] = item
-    return {"success": True, "item": item}
+async def create(item: ItemIn):
+    query = inventory.insert().values(**item.dict(by_alias=True))
+    inserted_id = await database.execute(query)
+    inserted_item = Item(**{"invID": inserted_id, **item.dict(by_alias=True)})
+    return {"success": True, "item": inserted_item}
 
 
 @app.get("/inventory")
-def retrieve(id: Optional[str] = None):
+async def retrieve(id: Optional[str] = None):
     if id:
-        return {"results": [data_store[id]]}
-    return {"results": list(data_store.values())}
+        query = inventory.select(inventory.c.invID == id)
+    else:
+        query = inventory.select()
+    return {"results": await database.fetch_all(query)}
+
+
+@app.get("/inventory/{item_id}")
+async def get_item(item_id: str):
+    query = inventory.select(inventory.c.invID == item_id)
+    return {"result": await database.fetch_one(query)}
 
 
 @app.delete("/inventory/{item_id}")
-def delete():
-    return {"Hello": "World"}
+async def delete(item_id: str):
+    query = inventory.delete(inventory.c.invID == item_id)
+    await database.execute(query)
+    return {"success": True}
+
+
+class CommentIn(BaseModel):
+    entry_date: Optional[datetime] = Field(
+        alias="entryDate", default_factory=lambda: datetime.now()
+    )
+    entered_by: str = Field(alias="enteredBy")
+    comment: str
+
+
+class Comment(CommentIn):
+    inv_id: int = Field(alias="invID")
+    id: int = Field(alias="commentID")
 
 
 @app.post("/inventory/comment/{item_id}")
-def comment():
-    return {"Hello": "World"}
+async def comment(item_id: str, comment: CommentIn):
+    query = inventory_comments.insert().values(
+        invID=item_id, **comment.dict(by_alias=True)
+    )
+    inserted_id = await database.execute(query)
+    return {
+        "success": True,
+        "comment": Comment(
+            **{"invID": item_id, "commentID": inserted_id},
+            **comment.dict(by_alias=True),
+        ),
+    }
+
+
+@app.get("/inventory/comment/{item_id}")
+async def get_comments(item_id: str):
+    query = inventory_comments.select(inventory_comments.c.invID == item_id)
+    return {"results": await database.fetch_all(query)}
 
 
 @app.delete("/inventory/comment/{comment_id}")
-def delete_comment():
+async def delete_comment():
     return {"Hello": "World"}
